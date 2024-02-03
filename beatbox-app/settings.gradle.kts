8b99205bed19f5e1c98d61f94b515a3d9957f0c3
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2024-02-03 19:27:44 +0800

    Fix for Gradle Build

--- beatbox-app/settings.gradle.kts
+++ beatbox-app/settings.gradle.kts
@@ -1,3 +1,2 @@
 rootProject.name = "beatbox-app"
-include("beatbox-app")
 
--- beatbox-app/src/main/java/org/ruralnative/Main.java
+++ beatbox-app/src/main/java/org/ruralnative/Main.java
@@ -3,5 +3,7 @@ package org.ruralnative;
 public class Main {
     public static void main(String[] args) {
         System.out.println("HI I am John Berlin");
+        MusicTest1 test = new MusicTest1();
+        test.play();
     }
 }
