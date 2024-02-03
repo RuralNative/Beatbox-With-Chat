14212662b1c8f8a1e59ed7266fbf4aae740b2f29
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2024-02-03 16:33:02 +0800

    Add Exception Handling
    Handle the possible exceptions that might be thrown by using a Sequencer object

--- beatbox-app/src/main/java/org/ruralnative/MusicTest1.java
+++ beatbox-app/src/main/java/org/ruralnative/MusicTest1.java
@@ -1,13 +1,18 @@
 package org.ruralnative;
 
-import javax.sound.midi;
 import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Sequencer;
 
 public class MusicTest1 {
 
     public void play() {
-        Sequencer sequencer = MidiSystem.getSequencer();
+        try {
+            Sequencer sequencer = MidiSystem.getSequencer();
+        } catch (MidiUnavailableException m) {
+            System.out.println("MidiUnavailableException THROWN!");
+            System.out.println("Please check if MIDI device is used by another process.");
+        }
         System.out.println("Initialized SEQUENCER");
     }
 }
