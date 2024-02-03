81485e60514165467f067f113a710b549a4928f8
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2024-02-03 20:46:41 +0800

    Add Codebase for Single Note
    Add codebase to add a singular note for the Sequencer to play from as a sample

--- beatbox-app/src/main/java/org/ruralnative/MusicTest1.java
+++ beatbox-app/src/main/java/org/ruralnative/MusicTest1.java
@@ -1,17 +1,37 @@
 package org.ruralnative;
 
-import javax.sound.midi.MidiSystem;
-import javax.sound.midi.MidiUnavailableException;
-import javax.sound.midi.Sequencer;
+import javax.sound.midi.*;
 
 public class MusicTest1 {
 
     public void play() {
         try {
-            Sequencer sequencer = MidiSystem.getSequencer();
+            Sequencer player = MidiSystem.getSequencer();
+            player.open();
+
+            Sequence sequence = new Sequence(Sequence.PPQ, 4);
+
+            Track track = sequence.createTrack();
+
+            ShortMessage messageOne = new ShortMessage();
+            messageOne.setMessage(144, 1, 44, 100);
+            MidiEvent noteOn = new MidiEvent(messageOne, 1);
+            track.add(noteOn);
+
+            ShortMessage messageTwo = new ShortMessage();
+            messageTwo.setMessage(128, 1, 44, 100);
+            MidiEvent noteOff = new MidiEvent(messageTwo, 16);
+            track.add(noteOff);
+
+            player.setSequence(sequence);
+
+            player.start();
         } catch (MidiUnavailableException m) {
             System.out.println("MidiUnavailableException THROWN!");
             System.out.println("Please check if MIDI device is used by another process.");
+        } catch (InvalidMidiDataException i) {
+            System.out.println("InvalidMidiDataException THROWN!");
+            System.out.println("Please check if Sequence data is valid.");
         }
         System.out.println("Initialized SEQUENCER");
     }
