f77723f71050a09584b45b8918f8016f3451e34c
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2024-02-03 16:26:31 +0800

    Update Main Class
    Added code to initialize Music Test 1 Class

--- beatbox-app/src/main/java/org/ruralnative/Main.java
+++ beatbox-app/src/main/java/org/ruralnative/Main.java
@@ -2,6 +2,7 @@ package org.ruralnative;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MusicTest1 musicTest = new MusicTest1();
+        musicTest.play();
     }
 }
