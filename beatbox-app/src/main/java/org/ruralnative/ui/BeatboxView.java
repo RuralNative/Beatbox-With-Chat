46a4c989531fa03877f991280a3c097994de76fc
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2024-02-04 11:25:41 +0800

    Migrated Beatbox UI to View Class
    Migrated the Beatbox UI codebase to View class as implementation of MVC

--- beatbox-app/src/main/java/org/ruralnative/ui/BeatboxView.java
+++ beatbox-app/src/main/java/org/ruralnative/ui/BeatboxView.java
@@ -1,4 +1,23 @@
 package org.ruralnative.ui;
 
-public class BeatboxView {
+import javax.swing.*;
+
+public class BeatboxView extends JFrame {
+
+    private JButton button;
+
+    BeatboxView() {
+        super("Beat Box App");
+        button = new JButton("Click Me");
+        add(button);
+
+        setSize(300, 300);
+        pack();
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    protected JButton getButton() {
+        return this.button;
+    }
 }
