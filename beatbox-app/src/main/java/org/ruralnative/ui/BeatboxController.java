3195026922e4452481923bb4effe1f9de344f696
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2024-02-04 11:35:57 +0800

    Create MidiPlayer Class
    Created MidiPlayer class and relocated MIDI functionality codebase from Controller to that class for separation of concerns

--- beatbox-app/src/main/java/org/ruralnative/ui/BeatboxController.java
+++ beatbox-app/src/main/java/org/ruralnative/ui/BeatboxController.java
@@ -1,4 +1,19 @@
 package org.ruralnative.ui;
 
+import javax.sound.midi.*;
+
 public class BeatboxController {
+
+    private BeatBoxModel model;
+    private BeatboxView view;
+
+    public BeatboxController() {
+        this.model = new BeatBoxModel();
+        this.view = new BeatboxView();
+        view.getButton().addActionListener(e -> handleButton());
+    }
+
+    private void handleButton() {
+        MidiPlayer.play();
+    }
 }
