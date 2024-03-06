002544442a8ae3e716432ea86d8a96e008994070
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2024-03-06 10:20:37 +0800

    Update Main for Socket Test
    Updated the codebase for the Main class to test  out the functionality of the Netwrok package classes

--- beatbox-app/src/main/java/org/ruralnative/Main.java
+++ beatbox-app/src/main/java/org/ruralnative/Main.java
@@ -1,10 +1,15 @@
 package org.ruralnative;
 
 
+import org.ruralnative.network.ChatClient;
+import org.ruralnative.network.ChatServer;
 import org.ruralnative.ui.beatbox.BeatboxView;
 
 public class Main {
     public static void main(String[] args) {
-        new BeatboxView();
+        new ChatServer();
+        ChatClient client = new ChatClient("127.0.0.1", 4000);
+        String message = client.readMessage();
+        System.out.println(message);
     }
 }
